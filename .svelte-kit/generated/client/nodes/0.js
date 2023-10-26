3d5c72f5ac2f3151a2716285c3e1470a6cca3bdf
John Hall <johngradyh@gmail.com>
2023-10-25 20:12:14 -0400

    initial commit

---
+++ .svelte-kit/generated/client/nodes/0.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/+layout.ts";
+export { universal };
+export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@1.20.4_svelte@4.0.5_vite@4.4.2/node_modules/@sveltejs/kit/src/runtime/components/layout.svelte";
